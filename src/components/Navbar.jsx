3010b55b91db56de1a30a476007f90fec52161dd
Oleksandra Masiukevych <60195328+SeggyFault@users.noreply.github.com>
2025-04-24 14:40:32 +0200

    style: Navbar component (closes #5)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,21 @@
 import { Link, NavLink } from 'react-router-dom'
+import { IoMenu } from "react-icons/io5";
 
 export default function Navbar() {
   return (
-    <nav className='flex gap-4 justify-center align-middle text-white bg-black'>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/contacts">Contacts</NavLink>
+
+    <nav className='flex justify-around items-center text-white bg-brand-primary h-20 w-full'>
+      <NavLink to="/" className="flex gap-4">
+        <img src="public/images/logo.svg" alt="Dezignlabs Logo Icon" className='h-7'/>
+        <img src="public/images/Dezignlabs.svg" alt="Dezignlabs Logo Text" />
+      </NavLink>
+      {/* Mobile Nav */}
+      <IoMenu className='text-4xl md:hidden'/>
+      {/* Desktop Nav */}
+      <div className='hidden md:flex justify-around items-baseline gap-4'>
+        <NavLink to="/" className="hover:text-brand-accent">Home</NavLink>
+        <NavLink to="/contacts" className="hover:text-brand-accent">Contacts</NavLink>
+      </div>
     </nav>
   )
 }
