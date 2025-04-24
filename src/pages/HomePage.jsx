073d06e8e2b8dc92f2b85eb6457880d354d21947
sggyflt <sgflt88@gmail.com>
2025-04-24 11:15:48 +0200

    chore: integrate Work Sans font (closes #18)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div>
-      <h1 className="text-9xl text-indigo-800">Dezignlabs</h1>
+      <h1 className="text-9xl text-indigo-800 font-work-sans">Dezignlabs</h1>
       <div id="box" className="bg-red-500 w-12 h-12"></div>
     </div>
   )
