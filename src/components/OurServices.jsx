0da1596df2def22e8c6a237153091db49ab0f0d5
sggyflt <sgflt88@gmail.com>
2025-04-27 17:45:56 +0200

    fix: node version in deploy.yml

---
+++ src/components/OurServices.jsx
@@ -0,0 +1,10 @@
+export default function OurServices() {
+  return (
+    <section className="bg-brand-primary">
+      <div className="container mx-auto">
+        <h2 className="text-text-on-dark uppercase text-5xl font-bold">Our services</h2>
+        <div></div>
+      </div>
+    </section>
+  )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import Hero from "../components/Hero"
+import OurServices from "../components/OurServices"
 import OurClients from "../components/OurClients"
 import CTA from "../components/CTA"
 
@@ -6,6 +7,7 @@ export default function HomePage() {
   return (
     <div>
       <Hero />
+      <OurServices />
       <OurClients />
       <CTA />
       {/*<div id="box" className="bg-red-500 w-12 h-12"></div>*/}
