0a7d6cde27d4839150043aec1c6a96c55128f2ae
Oleksandra Masiukevych <60195328+SeggyFault@users.noreply.github.com>
2025-05-02 09:18:53 +0200

    feat: create our services section to HomePage (closes #17)
    * feat: add grid structure to OurServices.jsx component
    
    * feat: create our services section to HomePage (closes #17)

---
+++ src/components/Card.jsx
@@ -0,0 +1,41 @@
+import Button from "./Button"
+
+export default function Card({ title, description, image, video, buttonText }) {
+  const videoId = video ? video.split("v=")[1]?.split("&")[0] : null;
+  return (
+    <div className="rounded-lg shadow-lg overflow-hidden">
+      <div className="relative w-full">
+        {/* Video or Image */}
+        {videoId ? (
+          <div className="relative w-full pb-[56.25%]">
+            <iframe 
+            className="absolute top-0 left-0 w-full h-full"
+            width="600"
+            height="400"
+            src={`https://www.youtube.com/embed/${videoId}`}
+            title="YouTube video player" 
+            frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerPolicy="strict-origin-when-cross-origin" 
+            allowFullScreen
+            loading="lazy"
+            ></iframe>
+          </div>
+        ) : image ? (
+          <img className="w-full h-64 object-cover" src={image} alt={title} />
+        ) : (
+          // Error message if no video
+        <p className="text-red-500">The video is not displayed.</p>
+        )
+      }
+        <div className="p-4 bg-surface-default flex flex-col gap-2">
+          <h3 className="text-xl font-semibold">{title}</h3>
+          <p className="text-gray-700">{description}</p>
+          {buttonText && (
+          <Button variant="cta" className="mx-auto ml-0">{buttonText}</Button>
+        )}
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/OurServices.jsx
+++ src/components/OurServices.jsx
@@ -1,9 +1,67 @@
+import Card from "./Card"
+
 export default function OurServices() {
   return (
-    <section className="bg-brand-primary">
-      <div className="container mx-auto">
+    <section className="bg-brand-primary py-12 md:py-20">
+      <div className="container px-4 md:px-0 mx-auto flex flex-col gap-4 md:gap-8">
         <h2 className="text-text-on-dark uppercase text-5xl font-bold">Our services</h2>
-        <div></div>
+        {/*Grid*/}
+        <div className="grid grid-cols-12 gap-4">
+          {/*Row 1*/}
+          <div className="col-span-12 md:col-span-6">
+            <Card
+              title="Marvel"
+              description="Promo video"
+              video="https://www.youtube.com/watch?v=CfEcO-ajjqE"
+            />
+          </div>
+          <div className="col-span-12 md:col-span-6">
+            <Card
+              title="Scrabble"
+              description="Promo video"
+              video="https://www.youtube.com/watch?v=WMOTWB-EPGs"
+            />
+          </div>
+          {/*Row 2*/}
+          <div className="col-span-12 md:col-span-8">
+            <Card
+              title="UX/UI Design"
+              description="Product design"
+              image="/images/card_3.webp"
+            />
+          </div>
+          <div className="col-span-12 md:col-span-4">
+             <Card
+              title="Static"
+              description="Promo design"
+              image="/images/card_4.webp"
+            />
+          </div>
+          {/*Row 3*/}
+          <div className="col-span-12 md:col-span-4">
+            <Card
+              title="Game Concepts"
+              description="Product design"
+              image="/images/card_5.webp"
+            />
+          </div>
+          <div className="col-span-12 md:col-span-8">
+             <Card
+              title="Work with influencers"
+              description="Communities, Promo design (Mr.Beast, etc)"
+              image="/images/card_6.webp"
+            />
+          </div>
+          {/*Row 4*/}
+          <div className="col-span-12">
+            <Card
+              title="E-paper"
+              description="Development, Product design"
+              image="/images/card_5.webp"
+              buttonText="Learn More"
+            />
+          </div>
+        </div>
       </div>
     </section>
   )
