ead3fdcb7032f5faad81af01f68d8a5926985013
Oleksandra Masiukevych <60195328+SeggyFault@users.noreply.github.com>
2025-05-06 14:30:42 +0200

    feat: create examples section for ePaper Design page (#39)

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -2,7 +2,7 @@ import Button from "./Button"
 
 export default function CTA({ title, buttonText, onClick }) {
   return (
-    <section className="bg-gradient-to-b from-brand-accent to-brand-primary py-12 md:py-20">
+    <section className="bg-gradient-to-b from-brand-primary to-brand-accent py-12 md:py-20">
       <div className="container mx-auto flex flex-col justify-center items-center gap-4 md:gap-8">
         <h2 className="uppercase text-3xl md:text-5xl font-bold text-text-default text-center">{title}</h2>
         <Button 
--- src/pages/EpaperPage.jsx
+++ src/pages/EpaperPage.jsx
@@ -7,6 +7,35 @@ export default function EpaperPage() {
 
   return (
     <div>
+      <section className="bg-brand-primary py-12 md:py-20">
+        <div className="container px-4 md:px-0 mx-auto flex flex-col gap-4 md:gap-8">
+          <h2 className="text-text-on-dark uppercase text-5xl font-bold">Examples</h2>
+          {/*Grid*/}
+          <div className="grid grid-cols-12 gap-4">
+            {/*Row 1*/}
+            <div className="col-span-12 md:col-span-4">
+              <img className="w-full h-80 object-cover" src="/images/example-1.jpg" alt="" />
+            </div>
+            <div className="col-span-12 md:col-span-8">
+              <img className="w-full h-80 object-cover" src="/images/example-2.jpg" alt="" />
+            </div>
+            {/*Row 2*/}
+            <div className="col-span-12 md:col-span-8">
+              <img className="w-full h-100 object-cover" src="/images/example-3.jpg" alt="" />
+            </div>
+            <div className="col-span-12 md:col-span-4">
+              <img className="w-full h-100 object-cover" src="/images/example-4.jpg" alt="" />
+            </div>
+            {/*Row 3*/}
+            <div className="col-span-12 md:col-span-6">
+              <img className="w-full h-80 object-cover" src="/images/example-5.jpg" alt="" />
+            </div>
+            <div className="col-span-12 md:col-span-6">
+              <img className="w-full h-80 object-cover" src="/images/example-6.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
       <CTA 
         title="Ready to elevate your e-paper experience?"
         buttonText="Let's Talk"
