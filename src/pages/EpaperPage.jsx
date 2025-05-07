36f1989191990027626596f84968cdf49980c9d5
Oleksandra Masiukevych <60195328+SeggyFault@users.noreply.github.com>
2025-05-07 09:59:18 +0200

    feat: add animation to examples section for ePaper Design page (closes #41)

--- src/pages/EpaperPage.jsx
+++ src/pages/EpaperPage.jsx
@@ -1,6 +1,7 @@
 import CTA from "../components/CTA"
 import { useNavigate } from 'react-router-dom'
 import ListItem from "../components/ListItem"
+import Examples from "../components/Examples"
 
 export default function EpaperPage() {
 
@@ -110,35 +111,9 @@ export default function EpaperPage() {
       </section>
 
       {/*EXAMPLES*/}
-      <section className="bg-brand-primary py-12 md:py-20">
-        <div className="container px-4 md:px-0 mx-auto flex flex-col gap-4 md:gap-8">
-          <h2 className="text-text-on-dark uppercase text-5xl font-bold tracking-wide">Examples</h2>
-          {/*Grid*/}
-          <div className="grid grid-cols-12 gap-4">
-            {/*Row 1*/}
-            <div className="col-span-12 md:col-span-4">
-              <img className="w-full h-80 object-cover" src="/images/example-1.jpg" alt="lidl advertisement" />
-            </div>
-            <div className="col-span-12 md:col-span-8">
-              <img className="w-full h-80 object-cover" src="/images/example-2.jpg" alt="info indoor billboard with directional signs" />
-            </div>
-            {/*Row 2*/}
-            <div className="col-span-12 md:col-span-8">
-              <img className="w-full h-100 object-cover" src="/images/example-3.jpg" alt="airport departure info board" />
-            </div>
-            <div className="col-span-12 md:col-span-4">
-              <img className="w-full h-100 object-cover" src="/images/example-4.jpg" alt="ikea advertisement" />
-            </div>
-            {/*Row 3*/}
-            <div className="col-span-12 md:col-span-6">
-              <img className="w-full h-80 object-cover" src="/images/example-5.jpg" alt="ikea happy valentine's adbertisment" />
-            </div>
-            <div className="col-span-12 md:col-span-6">
-              <img className="w-full h-80 object-cover" src="/images/example-6.jpg" alt="aldi coming soon billboard advertisment" />
-            </div>
-          </div>
-        </div>
-      </section>
+      <Examples />
+
+      {/*CTA*/}
       <CTA 
         title="Ready to elevate your e-paper experience?"
         buttonText="Let's Talk"
